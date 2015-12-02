90da94a8db09b74696e10690ff654f22a936b73e
Sean Feldman <feldman.sean@gmail.com>
2015-12-01 23:17:17 -0700

    windows title / size / position

---
+++ MiniVillain/Shared.Messages/SetupConsoleWindow.cs
@@ -0,0 +1,18 @@
+﻿namespace Shared
+{
+    using System;
+    using System.Configuration;
+    using NServiceBus;
+    using NServiceBus.Settings;
+
+    class SetupConsoleWindow : IWantToRunBeforeConfigurationIsFinalized
+    {
+        public void Run(SettingsHolder settings)
+        {
+
+            Console.Title = ConfigurationManager.AppSettings["minion.name"] ?? "Gru";
+            Console.SetWindowSize(50, 30);
+            Console.SetWindowPosition(0,0);
+        }
+    }
+}
